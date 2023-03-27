a9eee0de05a52dd4f55583601bd7ca12848c5210
ojer <sonamu@foxmail.com>
2023-03-27 10:34:44 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,17 +1,11 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
+import Vue from 'vue'
 
 import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
 
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-app.use(ElementPlus, { size: 'mini', zIndex: 3000 })
-
-app.mount('#app')
+new Vue({
+  router,
+  render: (h) => h(App)
+}).$mount('#app')
