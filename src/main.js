5e31131f3b331e17fe96caa4a3d033daa0fe2d8f
ojer <sonamu@foxmail.com>
2023-03-27 07:14:05 +0000

    up2

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
-
+// import './assets/main.css'
+Vue.use(ElementUI)
 new Vue({
   router,
   render: (h) => h(App)
